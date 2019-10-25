10d46e9e27fb2a5716221632a5373cb2355e4102
Philip Wahlgren <phwahlgren@gmail.com>
2019-10-25 14:33:11 +0200

    Full project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,50 +8,49 @@ namespace Calculator
         {
             Calculate(0);
         }
-        private static void Calculate(double startNumber)
+
+        static bool keepAlive = true;
+        private static void Calculate(double firstNumber)
         {
-            bool keepAlive = true;
             while (keepAlive)
             {
                 try 
                 {
-                    double firstNumber;
-                    if (startNumber == 0)
+                    Console.WriteLine("Calculator: press C to clear or E to exit");
+                    if (firstNumber == 0)
                     {
-                        Console.WriteLine("Enter a Number: ");
-                        firstNumber = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Type a number");
+                        firstNumber = double.Parse(newInput());
                     }
                     else
                     {
-                        firstNumber = startNumber;
                         Console.WriteLine(firstNumber);
                     }
-                    Console.WriteLine("Chose between +, -, /, * or C to clear");
-                    string userInput = Console.ReadLine();
+
+                    Console.WriteLine("Type +, -, /, *");
+                    string caseInput = newInput();
                     Console.WriteLine("Add another number");
-                    double secondNumber = double.Parse(Console.ReadLine());
+                    double secondNumber = double.Parse(newInput());
                     Console.Clear();
 
-                    switch (userInput)
+                    switch (caseInput)
                     {
                         case "+":
                             firstNumber = CalcAdd(firstNumber, secondNumber);
-                            Console.WriteLine(firstNumber);
                             break;
 
                         case "-":
                             firstNumber = CalcSub(firstNumber, secondNumber);
-                            Console.WriteLine(firstNumber);
                             break;
 
                         case "/":
                             if (secondNumber != 0)
                             {
                                 firstNumber = CalcDiv(firstNumber, secondNumber);
-                                Console.WriteLine(firstNumber);
                             }
                             else
                             {
+                                Console.Clear();
                                 Console.WriteLine("Cannot divide by 0");
                                 CalcClear(firstNumber);
                             }
@@ -59,14 +58,8 @@ namespace Calculator
 
                         case "*":
                             firstNumber = CalcMulti(firstNumber, secondNumber);
-                            Console.WriteLine(firstNumber);
                             break;
-
-                        case "C":
-                            Console.WriteLine("Clearing");
-                            CalcClear(0);
-                            break;
-
+                           
                         default:
                             Console.WriteLine("That is not an option");
                             CalcClear(firstNumber);
@@ -75,8 +68,33 @@ namespace Calculator
                 }
                 catch
                 {
-                    Console.WriteLine("CATCH!! That is not an option");
+                    Console.Clear();
+                    Console.WriteLine("That is not an option");
+                    CalcClear(firstNumber);
                 }
+            }
+        }
+        static string newInput()
+        {
+            string userInput = Console.ReadLine();
+            if ((userInput == "C") || userInput == ("c"))
+            {
+                Console.Clear();
+                Console.WriteLine("Clearing");
+                keepAlive = false;
+                CalcClear(0);
+                return "";
+            }
+            else if ((userInput == "E") || (userInput == "e"))
+            {
+                Console.WriteLine("Exiting calculator");
+                System.Threading.Thread.Sleep(1000);
+                Environment.Exit(0);
+                return ("");
+            }
+            else
+            {
+                return userInput;
             }
         }
         static double CalcAdd(double firstNumber, double secondNumber)
